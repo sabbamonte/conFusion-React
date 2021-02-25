0bd5e9228e0392faba90dc7179b2b58343eb17ae
Simone Abbamonte <simone@Simones-MacBook-Air.local>
2021-02-25 15:33:55 -0500

    uncontrolled form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+    Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component {
@@ -7,10 +8,12 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen:false
         };
 
         this.toggleNav = this.toggleNav.bind(this)
+        this.toggleModal = this.toggleModal.bind(this);
     }
 
     toggleNav() {
@@ -18,6 +21,12 @@ class Header extends Component {
             isNavOpen: !this.state.isNavOpen
         })
     }
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+      }
     render() {
         return(
         <React.Fragment>
@@ -50,7 +59,12 @@ class Header extends Component {
                         </NavLink>
                     </NavItem>
                 </Nav>
-                </Collapse>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                    </NavItem>
+                </Nav>
+                </Collapse>  
             </div>
         </Navbar>
         <Jumbotron>
@@ -61,8 +75,34 @@ class Header extends Component {
                         <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                     </div>
                 </div>
+                
             </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+            <ModalBody>
+                <Form onSubmit={this.handleLogin}>
+                    <FormGroup>
+                        <Label htmlFor="username">Username</Label>
+                        <Input type="text" id="username" name="username"
+                            innerRef={(input) => this.username = input} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor="password">Password</Label>
+                        <Input type="password" id="password" name="password"
+                            innerRef={(input) => this.password = input}  />
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                            <Input type="checkbox" name="remember"
+                            innerRef={(input) => this.remember = input}  />
+                            Remember me
+                        </Label>
+                    </FormGroup>
+                    <Button type="submit" value="submit" color="primary">Login</Button>
+                </Form>     
+            </ModalBody>
+        </Modal>
         </React.Fragment>
         );
     }
